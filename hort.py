9eddd4c1653f77dfab9c668ecdf107cabfc6a5e3
jhubler@heidelberg.edu <jhubler@cps330.heidelberg.edu>
2016-08-31 14:42:21 -0400

    Debugging

--- hort.py
+++ hort.py
@@ -6,12 +6,12 @@ import random
 numFlips = int(input("How many flips? "))
 
 flipCount = 0
-While (flipCount <= numFlips):
+while (numFlips >= flipCount):
     flip = random.randint(0,1)
 
     if (flip == 0):
         flipWord = "heads"
     else: 
         flipWord = "tails"
-
+    numFlips -= 1
     print("I flipped a coin and got", flipWord)
